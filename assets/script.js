e4f0be21c3fd116560b5b7a0e55c18fd9a05794b
Leonardo George <leonardogeorge30@gmail.com>
2025-02-15 14:56:53 -0300

    first commit

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+let dolar = 5.7277;
+
+let usdInput = document.querySelector("#usd");
+let brlInput = document.querySelector("#brl");
+
+
+usdInput.addEventListener("keyup", () => {
+
+} );
+
+brlInput.addEventListener("keyup", () => {
+
+} );
+
+usdInput.value = "1000,00";
+conversao("usd-to-brl");
+
+
+// Funções
+function conversao(type) {
+    if (type == "usd-to-brl") {
+        // Ajuste de valor ("," = "." USD);
+        // Converção; 
+        // Add valor de R$ Input;
+
+
+
+    }
+
+    if (type == "brl-to-usd") {
+        // Ajuste de valor ("," = "." USD);
+        // Converção; 
+        // Add valor de US$ Input;
+
+    }
+
+}

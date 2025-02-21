b3f1034357abce240efba43ed38d4a7bcc8dfb49
Leonardo George <leonardogeorge30@gmail.com>
2025-02-21 07:43:31 -0300

    Update JS

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,11 @@ let brlInput = document.querySelector("#brl");
 
 
 usdInput.addEventListener("keyup", () => {
-
+    usdInput.value = formatCurrency(usdInput.value)
 } );
 
 brlInput.addEventListener("keyup", () => {
-
+    brlInput.value = formatCurrency(brlInput.value)
 } );
 
 usdInput.value = "1000,00";
@@ -17,9 +17,34 @@ conversao("usd-to-brl");
 
 
 // Funções
-function conversao(type) {
+function formatCurrency(value) {
+    // Ajuste de valor...
+    let fixedValue = fixValue(value);
+    let options = {
+        useGrouping: false,
+        minimumFractuinDigits: 2
+    }
+
+    // Formatação;
+    let formatter = new Intl.NumberFormat("pr-BR", options)
+
+    // Return;
+    return formatter.format(fixedValue);
+
+}
+
+function fixValue (value) {
+    let fixedValue = value.replace(",", "."); // Ajuste de valor ("," = "." USD);
+    let floatValue = parseFloat(fixedValue);
+
+    if (floatValue == NaN) {
+        floatValue = 0
+    }
+    return floatValue
+}
+
+function convert(type) {
     if (type == "usd-to-brl") {
-        // Ajuste de valor ("," = "." USD);
         // Converção; 
         // Add valor de R$ Input;
 
